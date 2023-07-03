7fd6d3e96a07736fff0de21771ff1f03f992bd5e
GkhKaya <gkhkaya00@gmail.com>
2023-07-03 21:52:18 +0300

    Initial Commit

---
+++ MysteryOfNumbers/MysteryOfNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MysteryOfNumbersApp.swift
+//  MysteryOfNumbers
+//
+//  Created by Gokhan Kaya on 3.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MysteryOfNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
